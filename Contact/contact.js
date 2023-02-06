d4131482d9ecbcc36f6709c148d1f3eee523e7a2
pankaj-kb <pankajb2601@gmail.com>
2023-02-06 20:53:09 +0530

    adding vars

---
+++ Contact/contact.js
@@ -0,0 +1,7 @@
+let userNameInput = document.querySelector(".userName");
+
+let userEmailInput = document.querySelector(".userEmail");
+
+let userMessageInput = document.querySelector(".userMessage");
+
+let submitButton = document.querySelector(".mainRight button");
