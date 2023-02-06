23257a63c61d678a5c464eafb26f06a1974594d2
pankaj-kb <pankajb2601@gmail.com>
2023-02-06 21:56:49 +0530

    done till submitting the form

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,3 +1,5 @@
+// enter area
+
 let userNameInput = document.querySelector(".userName");
 
 let userEmailInput = document.querySelector(".userEmail");
@@ -5,3 +7,20 @@ let userEmailInput = document.querySelector(".userEmail");
 let userMessageInput = document.querySelector(".userMessage");
 
 let submitButton = document.querySelector(".mainRight button");
+
+// display Area
+
+let displayName = document.querySelector(".enterName");
+
+let displayEmail = document.querySelector(".enterMail");
+
+let displayMessage = document.querySelector(".enterMessage");
+
+let resetButton = document.querySelector(".mainLeft button");
+
+submitButton.addEventListener("click", () => {
+    event.preventDefault();
+    displayName.value = userNameInput.value;
+    displayEmail.value = userEmailInput.value;
+    displayMessage.value = userMessageInput.value;
+})
